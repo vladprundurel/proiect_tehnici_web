bd9d3b36f07fa1a91a2c61752ef4de2c154d4c21
vladprundurel <46630856+vladprundurel@users.noreply.github.com>
2019-01-12 19:34:15 +0200

    Add files via upload

---
+++ javascript.js
@@ -0,0 +1,690 @@
+function function_p1(){
+    var p1 = document.getElementById("p1");
+    p1.style.border="2px solid #00b300";
+    p1.style.borderRadius = "5px";
+    p1.style.opacity= "0.35";
+    
+}
+
+function function_p2(){
+    var p2 = document.getElementById("p2");
+    p2.style.border="2px solid #00b300";
+    p2.style.borderRadius = "5px";
+    p2.style.opacity= "0.35";
+}
+
+function function_p3(){
+    var p3 = document.getElementById("p3");
+    p3.style.border="2px solid #00b300";
+    p3.style.borderRadius = "5px";
+    p3.style.opacity= "0.35";
+}
+
+function function_p4(){
+    var p4 = document.getElementById("p4");
+    p4.style.border="2px solid #00b300";
+    p4.style.borderRadius = "5px";
+    p4.style.opacity= "0.35";
+}
+
+function function_p5(){
+    var p5 = document.getElementById("p5");
+    p5.style.border="2px solid #00b300";
+    p5.style.borderRadius = "5px";
+    p5.style.opacity= "0.35";
+}
+
+function function_p6(){
+    var p6 = document.getElementById("p6");
+    p6.style.border="2px solid #00b300";
+    p6.style.borderRadius = "5px";
+    p6.style.opacity= "0.35";
+}
+
+function function_p7(){
+    var p7 = document.getElementById("p7");
+    p7.style.border="2px solid #00b300";
+    p7.style.borderRadius = "5px";
+    p7.style.opacity= "0.35";
+}
+
+function function_p8(){
+    var p8 = document.getElementById("p8");
+    p8.style.border="2px solid #00b300";
+    p8.style.borderRadius = "5px";
+    p8.style.opacity= "0.35";
+}
+
+function function_p9(){
+    var p9 = document.getElementById("p9");
+    p9.style.border="2px solid #00b300";
+    p9.style.borderRadius = "5px";
+    p9.style.opacity= "0.35";
+}
+
+// --------------------------------------------------------
+
+function function_c1(){
+    var c1 = document.getElementById("c1");
+    c1.style.border="2px solid #00b300";
+    c1.style.borderRadius = "5px";
+    c1.style.opacity= "0.35";
+}
+
+function function_c2(){
+    var c2 = document.getElementById("c2");
+    c2.style.border="2px solid #00b300";
+    c2.style.borderRadius = "5px";
+    c2.style.opacity= "0.35";
+}
+
+function function_c3(){
+    var c3 = document.getElementById("c3");
+    c3.style.border="2px solid #00b300";
+    c3.style.borderRadius = "5px";
+    c3.style.opacity= "0.35";
+}
+
+function function_c4(){
+    var c4 = document.getElementById("c4");
+    c4.style.border="2px solid #00b300";
+    c4.style.borderRadius = "5px";
+    c4.style.opacity= "0.35";
+}
+
+function function_c5(){
+    var c5 = document.getElementById("c5");
+    c5.style.border="2px solid #00b300";
+    c5.style.borderRadius = "5px";
+    c5.style.opacity= "0.35";
+}
+
+function function_c6(){
+    var c6 = document.getElementById("c6");
+    c6.style.border="2px solid #00b300";
+    c6.style.borderRadius = "5px";
+    c6.style.opacity= "0.35";
+}
+
+function function_c7(){
+    var c7 = document.getElementById("c7");
+    c7.style.border="2px solid #00b300";
+    c7.style.borderRadius = "5px";
+    c7.style.opacity= "0.35";
+}
+
+function function_c8(){
+    var c8 = document.getElementById("c8");
+    c8.style.border="2px solid #00b300";
+    c8.style.borderRadius = "5px";
+    c8.style.opacity= "0.35";
+}
+
+function function_c9(){
+    var c9 = document.getElementById("c9");
+    c9.style.border="2px solid #00b300";
+    c9.style.borderRadius = "5px";
+    c9.style.opacity= "0.35";
+}
+
+// --------------------------------------------------------------------
+
+function function_f1(){
+    var f1 = document.getElementById("f1");
+    f1.style.border="2px solid #00b300";
+    f1.style.borderRadius = "5px";
+    f1.style.opacity= "0.35";
+}
+
+function function_f2(){
+    var f2 = document.getElementById("f2");
+    f2.style.border="2px solid #00b300";
+    f2.style.borderRadius = "5px";
+    f2.style.opacity= "0.35";
+}
+
+function function_f3(){
+    var f3 = document.getElementById("f3");
+    f3.style.border="2px solid #00b300";
+    f3.style.borderRadius = "5px";
+    f3.style.opacity= "0.35";
+}
+
+function function_f4(){
+    var f4 = document.getElementById("f4");
+    f4.style.border="2px solid #00b300";
+    f4.style.borderRadius = "5px";
+    f4.style.opacity= "0.35";
+}
+
+function function_f5(){
+    var f5 = document.getElementById("f5");
+    f5.style.border="2px solid #00b300";
+    f5.style.borderRadius = "5px";
+    f5.style.opacity= "0.35";
+}
+
+function function_f6(){
+    var f6 = document.getElementById("f6");
+    f6.style.border="2px solid #00b300";
+    f6.style.borderRadius = "5px";
+    f6.style.opacity= "0.35";
+}
+
+function function_f7(){
+    var f7 = document.getElementById("f7");
+    f7.style.border="2px solid #00b300";
+    f7.style.borderRadius = "5px";
+    f7.style.opacity= "0.35";
+}
+
+function function_f8(){
+    var f8 = document.getElementById("f8");
+    f8.style.border="2px solid #00b300";
+    f8.style.borderRadius = "5px";
+    f8.style.opacity= "0.35";
+}
+
+function function_f9(){
+    var f9 = document.getElementById("f9");
+    f9.style.border="2px solid #00b300";
+    f9.style.borderRadius = "5px";
+    f9.style.opacity= "0.35";
+}
+
+// -------------------------------------------------------------------
+
+function function_s1(){
+    var s1 = document.getElementById("s1");
+    s1.style.border="2px solid #00b300";
+    s1.style.borderRadius = "5px";
+    s1.style.opacity= "0.35";
+}
+
+function function_s2(){
+    var s2 = document.getElementById("s2");
+    s2.style.border="2px solid #00b300";
+    s2.style.borderRadius = "5px";
+    s2.style.opacity= "0.35";
+}
+
+function function_s3(){
+    var s3 = document.getElementById("s3");
+    s3.style.border="2px solid #00b300";
+    s3.style.borderRadius = "5px";
+    s3.style.opacity= "0.35";
+}
+
+function function_s4(){
+    var s4 = document.getElementById("s4");
+    s4.style.border="2px solid #00b300";
+    s4.style.borderRadius = "5px";
+    s4.style.opacity= "0.35";
+}
+
+function function_s5(){
+    var s5 = document.getElementById("s5");
+    s5.style.border="2px solid #00b300";
+    s5.style.borderRadius = "5px";
+    s5.style.opacity= "0.35";
+}
+
+function function_s6(){
+    var s6 = document.getElementById("s6");
+    s6.style.border="2px solid #00b300";
+    s6.style.borderRadius = "5px";
+    s6.style.opacity= "0.35";
+}
+
+function function_s7(){
+    var s7 = document.getElementById("s7");
+    s7.style.border="2px solid #00b300";
+    s7.style.borderRadius = "5px";
+    s7.style.opacity= "0.35";
+}
+
+function function_s8(){
+    var s8 = document.getElementById("s8");
+    s8.style.border="2px solid #00b300";
+    s8.style.borderRadius = "5px";
+    s8.style.opacity= "0.35";
+}
+
+function function_s9(){
+    var s9 = document.getElementById("s9");
+    s9.style.border="2px solid #00b300";
+    s9.style.borderRadius = "5px";
+    s9.style.opacity= "0.35";
+}
+
+
+var greutate, inaltime, nivel_activitate;
+function getValuesG()
+{
+    var g = document.getElementById("greutate");    
+    g.style.opacity = "0.5";
+    greutate = document.getElementById("greutate").value;
+    
+}
+
+function getValuesI()
+{
+    var i = document.getElementById("inaltime");
+    i.style.opacity = "0.5";
+    inaltime = document.getElementById("inaltime").value;
+}
+
+function Calculeaza(){
+    var n_a = document.getElementById("nivel_activitate");
+    n_a.style.opacity = "0.5";
+    nivel_activitate = document.getElementById("nivel_activitate").value;
+    var nrCal;
+    nrCal = greutate*2.2*nivel_activitate;
+    var precision = 10;
+    nrCal = Math.round(nrCal / precision) *  precision;
+ 
+    // ---- 
+    
+    
+    
+        // myRequest.send();
+    
+    // var myRequest = new XMLHttpRequest();
+    // myRequest.open("GET", "lose_weight.html");
+    // myRequest.onreadystatechange = function (){
+    //     if(myRequest.readyState === 4){
+    //         document.getElementById("").innerHTML = myRequest.responseText;
+    //     };
+    
+
+    // ----
+    var newContent = document.createTextNode("You need " + nrCal + " calories to keep your weight");
+    var currentElement = document.getElementById("null_content");
+    currentElement.appendChild(newContent);
+    currentElement.style.fontSize="30px";
+    currentElement.style.textDecoration= "underline";
+    
+}
+
+//----
+
+//---
+var array = [];
+var i=0;
+function clickedImgP(e)
+{
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    
+    if (e==p1)
+         {
+        console.log("piept de pui");
+                array[i] = "Chicken breast";
+         }
+    if (e==p2)
+         {
+         console.log("Tuna");
+                array[i] = "Tuna";
+         }
+    if (e==p3)
+         {
+        console.log("Beef");
+                array[i] = "Beef";
+         }
+    if (e==p4)
+         {
+        console.log("Pork muscles");
+                array[i] = "Pork muscles";
+         }
+    if (e==p5)
+         {
+         console.log("Cottage cheese");
+                array[i] = "Cottage cheese";
+         }
+    if (e==p6)
+         {
+        console.log("Turkey breast");
+                array[i] = "Turkey breast";
+         }
+    if (e==p7)
+         {
+        console.log("Eggs");
+                array[i] = "Eggs";
+         }
+    if (e==p8)
+         {
+         console.log("Salmon");
+                array[i] = "Salmon";
+         }
+    if (e==p9)
+         {
+        console.log("Soy");
+                array[i] = "Soy";
+         }
+    i++;
+
+    //console.log("ID: " + e.id);
+}
+
+
+
+// -------------------------------------------------------------------------
+var j=0;
+var arrayC = [];
+function clickedImgC(e)
+{
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    
+    if (e==c1)
+         {
+        console.log("Rice");
+                arrayC[j] = "Rice";
+         }
+    if (e==c2)
+         {
+         console.log("Potatoes");
+                arrayC[j] = "Potatoes";
+         }
+    if (e==c3)
+         {
+        console.log("Sweet potatoes");
+                arrayC[j] = "Sweet potatoes";
+         }
+    if (e==c4)
+         {
+        console.log("Whole pasta");
+                arrayC[j] = "Whole pasta";
+         }
+    if (e==c5)
+         {
+         console.log("Oatmeal");
+                arrayC[j] = "Oatmeal";
+         }
+    if (e==c6)
+         {
+        console.log("Rye bread");
+                arrayC[j] = "Rye bread";
+         }
+    if (e==c7)
+         {
+        console.log("Beans");
+                arrayC[j] = "Beans";
+         }
+    if (e==c8)
+         {
+         console.log("Quinoa");
+                arrayC[j] = "Quinoa";
+         }
+    if (e==c9)
+         {
+        console.log("Cornflakes");
+                arrayC[j] = "Cornflakes";
+         }
+    j++;
+
+    //console.log("ID: " + e.id);
+}
+// i--;
+// j--;
+
+//------------------------------------------------------------------
+
+var k=0;
+var arrayF = [];
+function clickedImgF(e)
+{
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    
+    if (e==f1)
+         {
+        console.log("Flaxseed");
+                arrayF[k] = "Flaxseed";
+         }
+    if (e==f2)
+         {
+         console.log("Avocado");
+                arrayF[k] = "Avocado";
+         }
+    if (e==f3)
+         {
+        console.log("Almonds");
+                arrayF[k] = "Almonds";
+         }
+    if (e==f4)
+         {
+        console.log("Peanuts");
+                arrayF[k] = "Peanuts";
+         }
+    if (e==f5)
+         {
+         console.log("Olive oil");
+                arrayF[k] = "Olive oil";
+         }
+    if (e==f6)
+         {
+        console.log("Nuts");
+                arrayF[k] = "Nuts";
+         }
+    if (e==f7)
+         {
+        console.log("Cashew");
+                arrayF[k] = "Cashew";
+         }
+    if (e==f8)
+         {
+         console.log("Olive");
+                arrayF[k] = "Olive";
+         }
+    if (e==f9)
+         {
+        console.log("Peanut butter");
+                arrayF[k] = "Peanut butter";
+         }
+    k++;
+
+    //console.log("ID: " + e.id);
+}
+
+//------------------------------------------------------------------------
+
+var l=0;
+var arrayS = [];
+function clickedImgS(e)
+{
+    e = e || window.event;
+    e = e.target || e.srcElement;
+    
+    if (e==s1)
+         {
+        console.log("Apples");
+                arrayS[l] = "Apples";
+         }
+    if (e==s2)
+         {
+         console.log("Bananas");
+                arrayS[l] = "Bananas";
+         }
+    if (e==s3)
+         {
+        console.log("Protein shake");
+                arrayS[l] = "Protein shake";
+         }
+    if (e==s4)
+         {
+        console.log("Protein bar");
+                arrayS[l] = "Protein bar";
+         }
+    if (e==s5)
+         {
+         console.log("Greek yoghurt");
+                arrayS[l] = "Greek yoghurt";
+         }
+    if (e==s6)
+         {
+        console.log("Dried fruits");
+                arrayS[l] = "Dried fruits";
+         }
+    if (e==s7)
+         {
+        console.log("Smoothie");
+                arrayS[l] = "Smoothie";
+         }
+    if (e==s8)
+         {
+         console.log("Dark chocolate");
+                arrayS[l] = "Dark chocolate";
+         }
+    if (e==s9)
+         {
+        console.log("Whole grain crackers");
+                arrayS[l] = "Whole grain crackers";
+         }
+    l++;
+}
+// function generateSnacks()
+// {
+
+//     var newCC = document.createTextNode("De asemenea, poti avea urmatoarele gustari intre mesele principale: ");
+//     var currEll = document.getElementById("propunere2");
+//     currEll.appendChild(newCC);
+//     currEll.style.fontSize="20px";
+
+//     for(l=0; l<arrayS.length; l++)
+//     {
+//         var listNodee = document.getElementById("list2");
+//         var liNodee = document.createElement("LI");
+//         var txtNodee = document.createTextNode(arrayS[l]);
+//         liNodee.appendChild(txtNodee);
+//         listNodee.appendChild(liNodee);
+
+//     }
+// }
+
+function generateDiet()
+{   
+    // location.href=("generate.html", "_blank");
+    // window.open("generate.html", "_blank");
+    var x;
+    var y;
+    var z;
+    var w;
+    for (x=0; x<array.length; x++)
+         {
+            console.log(array[x]);
+         }
+
+    var randomItemP = array[Math.floor(Math.random()*array.length)];
+
+    for (y=0; y<arrayC.length; y++)
+        {
+            console.log(arrayC[y]);
+        }
+    
+    var randomItemC = arrayC[Math.floor(Math.random()*arrayC.length)];
+
+    for (z=0; z<arrayF.length; z++)
+         {
+            console.log(arrayF[z]);
+         }
+
+    var randomItemF = arrayF[Math.floor(Math.random()*arrayF.length)];
+
+    for (w=0; w<arrayS.length; w++)
+         {
+            console.log(arrayS[w]);
+         }
+
+    var randomItemS = arrayS[Math.floor(Math.random()*arrayS.length)];
+
+    //console.log(randomItemP + " " + randomItemC + " " + randomItemF + " " + randomItemS);
+    //console.log(randomItemC);
+
+    var newC = document.createTextNode("Our proposal for meals according to your preferences is the following: ");
+    var currEl = document.getElementById("propunere");
+    currEl.appendChild(newC);
+    currEl.style.fontSize="30px";
+    currEl.style.textDecoration= "underline";
+
+    
+    var arrayFinal = [];
+    var arrayRandP = [100, 150, 200, 250];
+    var arrayRandC = [50, 60, 70, 80, 90, 100];
+    var arrayRandG = [10, 15, 20, 25, 30];
+    var q=0, rand_p;
+    var listNode = document.getElementById("list");
+    
+    for(i=0; i<array.length; i++)
+    {
+
+        rand_p = arrayRandP[Math.floor(Math.random()*arrayRandP.length)];
+        rand_c = arrayRandC[Math.floor(Math.random()*arrayRandC.length)];
+        rand_g = arrayRandG[Math.floor(Math.random()*arrayRandG.length)];
+        arrayFinal[q] = rand_p + " grams " + array[i] + ", " + rand_c + " grams " + arrayC[Math.floor(Math.random()*arrayC.length)] + ", " + rand_g + " grams " + arrayF[Math.floor(Math.random()*arrayF.length)];
+        
+        
+        var liNode = document.createElement("LI");
+        liNode.id="id_li";
+        var txtNode = document.createTextNode(arrayFinal[q]);
+
+        liNode.appendChild(txtNode);
+        listNode.appendChild(liNode);
+     
+        q++;
+        
+         id_li.style.background = "#dce5ff";
+         id_li.style.margin = "10px";
+         id_li.style.fontSize = "20px";
+         liNode.id="dss";
+          
+        
+        
+    }
+
+    for(l=0; l<arrayS.length; l++)
+    {
+        var liNodee = document.createElement("LI");
+        liNodee.id="id_lii";
+        var txtNodee = document.createTextNode(arrayS[l]);
+        liNodee.appendChild(txtNodee);
+        listNode.appendChild(liNodee);
+
+        id_lii.style.background = "#cce5ff";
+        // id_lii.style.margin = "5px";
+        id_lii.style.margin = "10px";
+        id_lii.style.fontSize = "20px";
+        list.style.listStyleType = "none";
+        liNodee.id="ds";
+
+    }
+}
+
+
+
+
+    // currEll.style.textDecoration= "underline";
+
+    
+
+    // for(q=0; q<arrayFinal.length; q++)
+    // {
+    //     var newC = document.createTextNode(arrayFinal[q]);
+    //     var currEl = document.getElementById("pid");
+    //     currEl.appendChild(newC);
+
+    // }
+
+
+   
+
+    
+    // var t = "100g " + randomItemP + " " + "100g " + randomItemC + " " + randomItemF + " " + randomItemS;
+    // var newC = document.createTextNode(t);
+    // var currEl = document.getElementById("pid");
+    // currEl.appendChild(newC);
+
+    
+
+    
+       
